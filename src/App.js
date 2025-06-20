ab3fbf424d6277a1d11d7f249111cf1fd79e0f91
LayMui <mui_ume@hotmail.com>
2025-06-20 17:22:32 +0800

    cleanup unused toaster

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Toaster } from "./components/ui/Toaster";
-import LandingPage from "./pages/Landing"; // Your main component or router
+import LandingPage from "./pages/Landing"; 
 import Header from "./components/ui/Header";
 import Projects from "./components/ui/Projects";
 import Contact from "./pages/Contact";
